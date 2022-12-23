ecc07be4f607ec6eedb3d55a09b556c0447b0664
TKachor <tkachor@gmail.com>
2022-12-23 13:54:27 +0200

    Changing the structure of the project

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,10 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,8 +14,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
