cced7a396ca951a75665caca2c4df32c683e3c2d
Pedchenko <just.lemonsafety@gmail.com>
2022-12-27 15:13:18 +0200

    update category model
    change type of id to GUID
    add validation (Image)
    add change value methods (All)
    remove extra whitespaces

--- Catalog.Domain/Entities/Category.cs
+++ Catalog.Domain/Entities/Category.cs
@@ -1,20 +1,18 @@
-﻿
-using Catalog.Domain.Objects;
+﻿using Catalog.Domain.Objects;
 
 namespace Catalog.Domain.Entities
 {
     public class Category
     {
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string Image { get; private set; }
-        
-        
         public ICollection<Product> Products { get; set; }
 
-
-        protected Category() { }
+        protected Category()
+        {
+        }
 
         public Category(string name, string description, string image)
         {
@@ -23,13 +21,32 @@ namespace Catalog.Domain.Entities
             Image = image;
 
             this.Validate();
-        }        
+        }
 
+        
+        public void ChangeName(string name)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(name, "Product description cannot be empty");
+            Name = name;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
+            Description = description;
+        }
+
+        public void ChangeImage(string image)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(image, "Product image cannot be empty");
+            Image = image;
+        }
+        
         private void Validate()
         {
             AssertionConcern.AssertArgumentNotEmpty(Name, "Category name cannot be empty");
-            AssertionConcern.AssertArgumentNotEmpty(Description, "Category Description cannot be empty");            
+            AssertionConcern.AssertArgumentNotEmpty(Description, "Category Description cannot be empty");
+            AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
         }
-
     }
 }
