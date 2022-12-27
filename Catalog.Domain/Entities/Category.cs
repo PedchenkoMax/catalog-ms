88386818854369495ca0f21e0f41f0a10956204f
TKachor <tkachor@gmail.com>
2022-12-27 23:31:19 +0200

    Add and update validation for category entity

--- Catalog.Domain/Entities/Category.cs
+++ Catalog.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.Domain.Exception;
+using System.Xml.Linq;
 
 namespace Catalog.Domain.Entities;
 
@@ -26,13 +27,15 @@ public class Category
         
     public void ChangeName(string name)
     {
-        AssertionConcern.AssertArgumentNotEmpty(name, "Product description cannot be empty");
+        AssertionConcern.AssertArgumentNotEmpty(name, "Category name cannot be empty");
+        AssertionConcern.AssertArgumentLength(name, 30, "Category name cannot be longer than 30 characters");
         Name = name;
     }
 
     public void ChangeDescription(string description)
     {
-        AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
+        AssertionConcern.AssertArgumentNotEmpty(description, "Category description cannot be empty");
+        AssertionConcern.AssertArgumentLength(description, 2000, "Category name cannot be longer than 2000 characters");
         Description = description;
     }
 
@@ -45,7 +48,9 @@ public class Category
     private void Validate()
     {
         AssertionConcern.AssertArgumentNotEmpty(Name, "Category name cannot be empty");
+        AssertionConcern.AssertArgumentLength(Name, 30, "Category name cannot be longer than 30 characters");
         AssertionConcern.AssertArgumentNotEmpty(Description, "Category Description cannot be empty");
+        AssertionConcern.AssertArgumentLength(Description, 2000, "Category name cannot be longer than 2000 characters");
         AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
     }
 }
