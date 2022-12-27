85e2a09670cb8fc8ad78889c72a7dc0ca534dc79
TKachor <tkachor@gmail.com>
2022-12-27 13:34:23 +0200

    Add model validation in Product and Category entities

--- Catalog.Domain/Entities/Category.cs
+++ Catalog.Domain/Entities/Category.cs
@@ -1,8 +1,11 @@
 ﻿
+using Catalog.Domain.Objects;
+
 namespace Catalog.Domain.Entities
 {
     public class Category
     {
+        public int Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string Image { get; private set; }
@@ -17,8 +20,16 @@ namespace Catalog.Domain.Entities
         {
             Name = name;
             Description = description;
-            Image = image;          
+            Image = image;
+
+            this.Validate();
+        }        
+
+        private void Validate()
+        {
+            AssertionConcern.AssertArgumentNotEmpty(Name, "Category name cannot be empty");
+            AssertionConcern.AssertArgumentNotEmpty(Description, "Category Description cannot be empty");            
         }
-      
+
     }
 }
--- Catalog.Domain/Entities/Product.cs
+++ Catalog.Domain/Entities/Product.cs
@@ -1,4 +1,6 @@
 ﻿
+using Catalog.Domain.Objects;
+
 namespace Catalog.Domain.Entities
 {
     public class Product
@@ -10,17 +12,20 @@ namespace Catalog.Domain.Entities
         public decimal Value { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public string Image { get; private set; }
-        public int StockQuantity { get; private set; }        
+        public int StockQuantity { get; private set; }
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; }
 
         protected Product() { }
 
         public Product(
+            Guid productId,
             string name,
             string description,
             bool active,
-            decimal value,
-            Guid productId,
-            string image
+            decimal value,            
+            string image,
+            int categoryId
             )
         {
             ProductId = productId;
@@ -29,8 +34,56 @@ namespace Catalog.Domain.Entities
             Active = active;
             Value = value;
             CreatedAt = DateTime.Now;
-            Image = image;           
+            Image = image;
+            CategoryId = categoryId;
+
+            this.Validate();
+        }
+
+        public void Activate() => Active = true;
+
+        public void Deactivate() => Active = false;
+
+        public bool HasStock(int quantity)
+        {
+            return StockQuantity >= quantity;
+        }
+
+        public void ChangeName(string name)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(name, "Product description cannot be empty");
+            Name = name;
+        }       
+
+        public void ChangeDescription(string description)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
+            Description = description;
+        }
+
+        public void ChangeValue(decimal value)
+        {
+            AssertionConcern.AssertArgumentGreaterThan(Value, 1, "Product value cannot be less than 1");
+            Value = value;
+        }
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        private void Validate()
+        {
+            AssertionConcern.AssertArgumentNotEmpty(Name, "Product name cannot be empty");
+            AssertionConcern.AssertArgumentNotEmpty(Description, "Product description cannot be empty");
+            AssertionConcern.AssertArgumentNotEquals(CategoryId, 0, "Product category id cannot be empty");
+            AssertionConcern.AssertArgumentGreaterThan(Value, 1, "Product value cannot be less than 1");
+            AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
         }
     }
 
+
 }
+
+
