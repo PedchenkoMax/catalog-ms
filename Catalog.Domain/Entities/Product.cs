2851fbea9a645bc296023f2ce3d60e8dc5edf771
Pedchenko <just.lemonsafety@gmail.com>
2022-12-27 15:50:16 +0200

    update product model
    rename value to price
    remove extra fields (CreatedAt)
    add change value methods (Image)
    rework HasStock
    remove extra whitespaces

--- Catalog.Domain/Entities/Product.cs
+++ Catalog.Domain/Entities/Product.cs
@@ -1,73 +1,51 @@
-﻿
-using Catalog.Domain.Objects;
+﻿using Catalog.Domain.Objects;
 
 namespace Catalog.Domain.Entities
 {
     public class Product
     {
         public Guid ProductId { get; private set; }
+        public Guid CategoryId { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
-        public string Brand { get; private set; }
-        public bool Active { get; private set; }
-        public decimal Value { get; private set; }
-        public DateTime CreatedAt { get; private set; }
+        public decimal Price { get; private set; }
         public string Image { get; private set; }
         public int StockQuantity { get; private set; }
-        public int CategoryId { get; private set; }        
-        public Category Category { get; private set; }        
+        public bool IsActive { get; private set; }
+        public Category Category { get; private set; }
 
-        protected Product() { }
+        protected Product()
+        {
+        }
 
         public Product(
             Guid productId,
+            Guid categoryId,
+            bool isActive,
             string name,
             string description,
-            string brand,
-            bool active,
-            decimal value,            
-            string image,
-            int categoryId
-            )
+            decimal price,
+            string image
+        )
         {
             ProductId = productId;
+            CategoryId = categoryId;
+            IsActive = isActive;
             Name = name;
             Description = description;
-            Brand = brand;
-            Active = active;
-            Value = value;
-            CreatedAt = DateTime.Now;
+            Price = price;
             Image = image;
-            CategoryId = categoryId;
 
             this.Validate();
         }
 
-        public void Activate() => Active = true;
+        public void Activate() => IsActive = true;
 
-        public void Deactivate() => Active = false;
+        public void Deactivate() => IsActive = false;
 
-        public bool HasStock(int quantity)
+        public bool HasStock()
         {
-            return StockQuantity >= quantity;
-        }
-
-        public void ChangeName(string name)
-        {
-            AssertionConcern.AssertArgumentNotEmpty(name, "Product description cannot be empty");
-            Name = name;
-        }       
-
-        public void ChangeDescription(string description)
-        {
-            AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
-            Description = description;
-        }
-
-        public void ChangeValue(decimal value)
-        {
-            AssertionConcern.AssertArgumentGreaterThan(Value, 1, "Product value cannot be less than 1");
-            Value = value;
+            return StockQuantity > 0;
         }
 
         public void ChangeCategory(Category category)
@@ -76,17 +54,37 @@ namespace Catalog.Domain.Entities
             CategoryId = category.Id;
         }
 
+        public void ChangeName(string name)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(name, "Product description cannot be empty");
+            Name = name;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
+            Description = description;
+        }
+
+        public void ChangePrice(decimal price)
+        {
+            AssertionConcern.AssertArgumentGreaterThan(price, 1, "Product price cannot be less than 1");
+            Price = price;
+        }
+
+        public void ChangeImage(string image)
+        {
+            AssertionConcern.AssertArgumentNotEmpty(image, "Product image cannot be empty");
+            Image = image;
+        }
+
         private void Validate()
         {
             AssertionConcern.AssertArgumentNotEmpty(Name, "Product name cannot be empty");
             AssertionConcern.AssertArgumentNotEmpty(Description, "Product description cannot be empty");
             AssertionConcern.AssertArgumentNotEquals(CategoryId, 0, "Product category id cannot be empty");
-            AssertionConcern.AssertArgumentGreaterThan(Value, 1, "Product value cannot be less than 1");
+            AssertionConcern.AssertArgumentGreaterThan(Price, 1, "Product price cannot be less than 1");
             AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
         }
     }
-
-
 }
-
-
