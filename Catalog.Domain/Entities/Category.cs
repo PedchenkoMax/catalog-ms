a9bcfd858c3661177a158d4ce1458f3046d1ffd3
Pedchenko <just.lemonsafety@gmail.com>
2023-01-09 00:53:26 +0200

    remove rich model specific code

--- Catalog.Domain/Entities/Category.cs
+++ Catalog.Domain/Entities/Category.cs
@@ -1,6 +1,4 @@
-﻿using Catalog.Domain.Exceptions;
-
-namespace Catalog.Domain.Entities;
+﻿namespace Catalog.Domain.Entities;
 
 public class Category
 {
@@ -9,47 +7,4 @@ public class Category
     public string Description { get; private set; }
     public string Image { get; private set; }
     public ICollection<Product> Products { get; set; }
-
-    protected Category()
-    {
-    }
-
-    public Category(string name, string description, string image)
-    {
-        Name = name;
-        Description = description;
-        Image = image;
-
-        this.Validate();
-    }
-
-        
-    public void ChangeName(string name)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(name, "Category name cannot be empty");
-        AssertionConcern.AssertArgumentLength(name, 30, "Category name cannot be longer than 30 characters");
-        Name = name;
-    }
-
-    public void ChangeDescription(string description)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(description, "Category description cannot be empty");
-        AssertionConcern.AssertArgumentLength(description, 2000, "Category name cannot be longer than 2000 characters");
-        Description = description;
-    }
-
-    public void ChangeImage(string image)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(image, "Product image cannot be empty");
-        Image = image;
-    }
-        
-    private void Validate()
-    {
-        AssertionConcern.AssertArgumentNotEmpty(Name, "Category name cannot be empty");
-        AssertionConcern.AssertArgumentLength(Name, 30, "Category name cannot be longer than 30 characters");
-        AssertionConcern.AssertArgumentNotEmpty(Description, "Category Description cannot be empty");
-        AssertionConcern.AssertArgumentLength(Description, 2000, "Category name cannot be longer than 2000 characters");
-        AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
-    }
 }
--- Catalog.Domain/Entities/Product.cs
+++ Catalog.Domain/Entities/Product.cs
@@ -1,93 +1,14 @@
-using Catalog.Domain.Exceptions;
-
 namespace Catalog.Domain.Entities;
 
 public class Product
 {
-    public Guid ProductId { get; private set; }
-    public Guid CategoryId { get; private set; }
-    public string Name { get; private set; }
-    public string Description { get; private set; }
-    public decimal Price { get; private set; }
-    public string Image { get; private set; }
-    public int StockQuantity { get; private set; }
-    public bool IsActive { get; private set; }
-    public Category Category { get; private set; }
-
-    protected Product()
-    {
-    }
-
-    public Product(
-        Guid productId,
-        Guid categoryId,
-        bool isActive,
-        string name,
-        string description,
-        decimal price,
-        string image
-    )
-    {
-        ProductId = productId;
-        CategoryId = categoryId;
-        IsActive = isActive;
-        Name = name;
-        Description = description;
-        Price = price;
-        Image = image;
-
-        this.Validate();
-    }
-
-    public void Activate() => IsActive = true;
-
-    public void Deactivate() => IsActive = false;
-
-    public bool HasStock()
-    {
-        return StockQuantity > 0;
-    }
-
-    public void ChangeCategory(Category category)
-    {
-        Category = category;
-        CategoryId = category.Id;
-    }
-
-    public void ChangeName(string name)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(name, "Product name cannot be empty");
-        AssertionConcern.AssertArgumentLength(name, 60, "Product name cannot be longer than 60 characters");
-        Name = name;
-    }
-
-    public void ChangeDescription(string description)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(description, "Product description cannot be empty");
-        AssertionConcern.AssertArgumentLength(description, 4000, "Product description cannot be longer than 4000 characters");
-        Description = description;
-    }
-
-    public void ChangePrice(decimal price)
-    {
-        AssertionConcern.AssertArgumentGreaterThan(price, 0.01M, "Product price cannot be less than 0.01");
-        Price = price;
-    }
-
-    public void ChangeImage(string image)
-    {
-        AssertionConcern.AssertArgumentNotEmpty(image, "Product image cannot be empty");
-        Image = image;
-    }
-
-    private void Validate()
-    {
-        AssertionConcern.AssertArgumentNotEmpty(Name, "Product name cannot be empty");
-        AssertionConcern.AssertArgumentLength(Name, 60, "Product name cannot be longer than 60 characters");
-        AssertionConcern.AssertArgumentNotEmpty(Description, "Product description cannot be empty");
-        AssertionConcern.AssertArgumentLength(Description, 4000, "Product description cannot be longer than 4000 characters");
-        AssertionConcern.AssertArgumentNotEmpty(CategoryId,"Product category id cannot be empty");
-        AssertionConcern.AssertArgumentGreaterThan(Price, 0.01M, "Product price cannot be less than 0.01");
-        AssertionConcern.AssertArgumentNotEmpty(Image, "Product image cannot be empty");
-    }
+    public Guid ProductId { get; set; }
+    public Guid CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Image { get; set; }
+    public int StockQuantity { get; set; }
+    public bool IsActive { get; set; }
+    public Category Category { get; set; }
 }
