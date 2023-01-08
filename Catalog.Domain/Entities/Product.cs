4a26bbbaaf8455e585647add11846cd0cad2b3a5
Pedchenko <just.lemonsafety@gmail.com>
2023-01-09 01:26:45 +0200

    adjust models according to "warehouse"

--- Catalog.Domain/Entities/Product.cs
+++ Catalog.Domain/Entities/Product.cs
@@ -3,12 +3,15 @@ namespace Catalog.Domain.Entities;
 public class Product
 {
     public Guid ProductId { get; set; }
-    public Guid CategoryId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
     public decimal Price { get; set; }
-    public string Image { get; set; }
-    public int StockQuantity { get; set; }
-    public bool IsActive { get; set; }
+    public string? Images { get; set; }
+    public string Description { get; set; }
+
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
+    
+    // public Guid ProviderId  { get; set; }
+    // public Provider Provider { get; set; }
 }
