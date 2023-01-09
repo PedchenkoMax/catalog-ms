dae22fd2e601a2a078f79a5cdaba825fdf98babd
Pedchenko <just.lemonsafety@gmail.com>
2023-01-09 04:13:06 +0200

    add simple product repository

---
+++ Catalog.Infrastructure/Database/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure.Database.Repositories;
+
+public class ProductRepository
+{
+    private readonly CatalogContext ctx;
+
+    public ProductRepository(CatalogContext ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    public List<Product> GetAll()
+    {
+        return ctx.Products.ToList();
+    }
+
+    public Product Get(Guid productId)
+    {
+        return ctx.Products
+            .First(x => x.ProductId == productId);
+    }
+
+    public int Add(Product newProduct)
+    {
+        ctx.Products.Add(newProduct);
+
+        return ctx.SaveChanges();
+    }
+
+    public int Remove(Product product)
+    {
+        ctx.Products.Remove(product);
+
+        return ctx.SaveChanges();
+    }
+}
