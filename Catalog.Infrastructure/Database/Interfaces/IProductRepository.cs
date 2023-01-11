a53c0c34c60476e4df06c5395bbf8923ff56f4a8
Pedchenko <just.lemonsafety@gmail.com>
2023-01-11 10:44:40 +0200

    add abstraction for product repository

---
+++ Catalog.Infrastructure/Database/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Domain.Entities;
+
+namespace Catalog.Infrastructure.Database.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid productId);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<IEnumerable<Product>> GetAllByCategoryAsync(int categoryId);
+}
