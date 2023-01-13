0e517183b1d9d944c87f0389411e7dc3d1d3b4c1
Pedchenko <just.lemonsafety@gmail.com>
2023-01-13 04:54:35 +0200

    feature: make fields (Quantity, Description) nullable

--- Catalog.Domain/Entities/Product.cs
+++ Catalog.Domain/Entities/Product.cs
@@ -4,10 +4,10 @@ public class Product
 {
     public Guid ProductId { get; set; }
     public string Name { get; set; }
-    public int Quantity { get; set; }
+    public int? Quantity { get; set; }
     public decimal Price { get; set; }
     public string? Image { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     public Guid CategoryId { get; set; }
     public Category Category { get; set; }
