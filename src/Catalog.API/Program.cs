80074920c7c053163f680a8de4ad0ac19b475149
TKachor <tkachor@gmail.com>
2023-01-23 23:17:48 +0200

    refactor: rewrite settings Dockerfile and docker-compose.yml

--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -13,11 +13,8 @@ var services = builder.Services;
 
 var app = builder.Build();
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseAuthorization();
 
