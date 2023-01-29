d091e836e0371b3213856baa7503e0b6ddab2765
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-01-29 04:01:53 +0200

    feature: add ProductImageEntity class

---
+++ src/Catalog.Domain/Entities/ProductImageEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Domain.Entities;
+
+public class ProductImageEntity
+{
+    public Guid ProductImageId { get; set; }
+    public string ImageUrl { get; set; }
+    public bool IsMain { get; set; }
+
+    public Guid ProductId { get; set; }
+    public ProductEntity ProductEntity { get; set; }
+}
