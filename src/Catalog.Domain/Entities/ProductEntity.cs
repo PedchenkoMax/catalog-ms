028e0ba06c9a6a6278e8283b65f732b1da0700fe
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-01-29 04:23:46 +0200

    refactor: reorder properties

--- src/Catalog.Domain/Entities/ProductEntity.cs
+++ src/Catalog.Domain/Entities/ProductEntity.cs
@@ -4,12 +4,12 @@ public class ProductEntity
 {
     public Guid ProductId { get; set; }
     public string Name { get; set; }
-    public int? Quantity { get; set; }
-    public bool IsActive { get; set; }
+    public string? Description { get; set; }
+    public IList<ProductImageEntity> Images { get; set; }
     public decimal Price { get; set; }
     public decimal Sale { get; set; }
-    public IList<ProductImageEntity> Images { get; set; }
-    public string? Description { get; set; }
+    public int? Quantity { get; set; }
+    public bool IsActive { get; set; }
 
     public Guid CategoryId { get; set; }
     public CategoryEntity CategoryEntity { get; set; }
