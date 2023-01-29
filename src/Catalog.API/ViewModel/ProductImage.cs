60d96a50919305fb34fac759ef3ebf963f88aadd
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-01-29 05:10:05 +0200

    chore: add ProductImage record

---
+++ src/Catalog.API/ViewModel/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Catalog.API.ViewModel;
+
+public record ProductImage
+{
+    [DefaultValue("https://blobstorage.com/default-image.jpg")]
+    public string ImageUrl { get; init; }
+
+    [DefaultValue(true)]
+    public bool IsMain { get; init; }
+}
