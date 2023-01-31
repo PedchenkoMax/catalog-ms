8d08068e4c1e430228d64c5fd8c1393590dd247e
TKachor <tkachor@gmail.com>
2023-02-01 00:14:19 +0200

    feature: add global http logging middleware

--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -1,5 +1,6 @@
 using Catalog.API.Middlewares;
 using Catalog.Infrastructure.Database;
+using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +11,15 @@ var services = builder.Services;
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
     services.AddDbContext<CatalogContext>(o => o.UseSqlServer(builder.Configuration["ConnectionString"]));
+    services.AddHttpLogging(l => l.LoggingFields = HttpLoggingFields.All);
 }
 
 var app = builder.Build();
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseHttpLogging();
 
     app.ConfigureExceptionHandler();
     
