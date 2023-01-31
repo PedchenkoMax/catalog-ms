2bb0d084f082a7535f154d4522c650ff03cc1f4c
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-01-31 02:31:43 +0200

    feature: add attribute for Guid id's validation

---
+++ src/Catalog.API/Validation/Attributes/GuidIdAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Validation.Attributes;
+
+/// <summary>
+/// Returns `true` if the input value is either `null` or a valid `Guid` that is not equal to `Guid.Empty`.
+/// Returns `false` if the input value is not `null` and is not a valid `Guid`.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property)]
+public class GuidIdAttribute : ValidationAttribute
+{
+    public GuidIdAttribute() : base("The value is not a valid guid id.")
+    {
+    }
+
+    public override bool IsValid(object? value)
+    {
+        if (value == null)
+            return true;
+
+        if (value is not Guid guidValue)
+            return false;
+
+        return guidValue != Guid.Empty;
+    }
+}
