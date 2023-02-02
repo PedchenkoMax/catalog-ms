4869540a6e2e51d5d5ea1c1ceb50190c0f5a86e1
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-03 01:50:38 +0200

    feature: add SeedDataConstants

---
+++ src/Catalog.Domain/Constants/SeedDataConstants.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.Domain.Constants;
+
+public static class SeedDataConstants
+{
+    //BrandId
+    public static readonly Guid BrandApple = Guid.Parse("d942706b-e4e2-48f9-bbdc-b022816471f0");
+    public static readonly Guid BrandSamsung = Guid.Parse("2cd4b9a0-f70d-476d-a3cc-908da43f93c4");
+    public static readonly Guid BrandLg = Guid.Parse("1F5AD630-32CD-42E9-8218-E26F1D375C0C");
+
+    //CategoryId
+    public static readonly Guid CategoryPhone = Guid.Parse("5e7274ad-3132-4ad7-be36-38778a8f7b1c");
+    public static readonly Guid CategoryTv = Guid.Parse("72a7a013-8bc4-4ae6-89cb-d9f19e0c9cf9");
+
+    // ProductId
+    public static readonly Guid Phone1 = Guid.Parse("5b515247-f6f5-47e1-ad06-95f317a0599b");
+    public static readonly Guid Phone2 = Guid.Parse("56d6294f-7b80-4a78-856a-92b141de2d1c");
+    public static readonly Guid Phone3 = Guid.Parse("e77cf2cb-3f3a-4f0b-ac5a-90a3263d075a");
+    public static readonly Guid Phone4 = Guid.Parse("fd3c67c5-c6ff-4a5d-a166-98ece1b7752b");
+    public static readonly Guid Phone5 = Guid.Parse("1e338b12-8aa6-438f-8832-8c7429805d59");
+
+    public static readonly Guid Tv1 = Guid.Parse("d1ae1de1-1aa8-4650-937c-4ed882038ad7");
+    public static readonly Guid Tv2 = Guid.Parse("FEAD0B48-9986-43CB-A5C0-14F91EB6DCDF");
+    public static readonly Guid Tv3 = Guid.Parse("9D52DA22-00DA-4C84-AB62-0C4279A332AF");
+    public static readonly Guid Tv4 = Guid.Parse("7BA99BE2-9284-4343-BC2F-8997A5A85B62");
+    public static readonly Guid Tv5 = Guid.Parse("D9B7D7C8-61BB-4CEE-AEC7-D6AA4B8B6315");
+}
