dc9fb26442bc96407ac530190db4191e54037fd6
TKachor <tkachor@gmail.com>
2023-02-07 21:57:54 +0200

    feature: add test with null value to GuidIdAttributeTests

---
+++ src/Catalog.Tests/ValidationAttribute/GuidIdAttributeTests.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Validation.Attributes;
+
+namespace Catalog.Tests.ValidationAttribute;
+
+public class GuidIdAttributeTests
+{
+    [Fact]
+    public void IsValid_WithNullValue_ReturnsTrue()
+    {       
+        var attribute = new GuidIdAttribute();
+        
+        var result = attribute.IsValid(null);
+       
+        Assert.True(result);
+    }
+    
+}
+
+
