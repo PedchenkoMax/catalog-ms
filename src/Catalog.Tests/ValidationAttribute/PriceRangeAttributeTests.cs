f72be32e9ce6feabf0c6f82a73504e9c085efcfb
TKachor <tkachor@gmail.com>
2023-02-07 23:00:59 +0200

    feature: add test min price less then max in PriceRangeAttributeTests

--- src/Catalog.Tests/ValidationAttribute/PriceRangeAttributeTests.cs
+++ src/Catalog.Tests/ValidationAttribute/PriceRangeAttributeTests.cs
@@ -60,5 +60,21 @@ public class PriceRangeAttributeTests
         Assert.False(isValid);       
         Assert.Equal("MinPrice must be lower than MaxPrice.", results[0].ErrorMessage);
     }
-    
+
+    [Fact]
+    public void MinPriceLessThanMaxPrice_ReturnsSuccess()
+    {
+        var model = new TestModel
+        {
+            MinPrice = 1,
+            MaxPrice = 10
+        };
+
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        Assert.True(isValid);
+        Assert.Empty(results);
+    }
 }
