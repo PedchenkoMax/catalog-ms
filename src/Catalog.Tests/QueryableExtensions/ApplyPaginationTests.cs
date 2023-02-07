46841cf649f7ee3af65558e6c68740718a3bf0cb
TKachor <tkachor@gmail.com>
2023-02-08 01:47:31 +0200

    feature: add test in ApplyPaginationTests to return correct page when paging is set

--- src/Catalog.Tests/QueryableExtensions/ApplyPaginationTests.cs
+++ src/Catalog.Tests/QueryableExtensions/ApplyPaginationTests.cs
@@ -11,7 +11,18 @@ public class ApplyPaginationTests
         Assert.Equal(10, result.Count());
         Assert.Equal(1, result.First());
         Assert.Equal(10, result.Last());
-    }        
-   
+    }
+        
+    [Fact]
+    public void ApplyPagination_ShouldReturnCorrectPage_WhenPaginationIsSet()
+    {
+        var items = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}.AsQueryable();
+
+        var result = items.ApplyPagination(new PaginationFilter(1,3));
+
+        Assert.Equal(3, result.Count());
+        Assert.Equal(4, result.First());
+        Assert.Equal(6, result.Last());
+    }    
 }
 
