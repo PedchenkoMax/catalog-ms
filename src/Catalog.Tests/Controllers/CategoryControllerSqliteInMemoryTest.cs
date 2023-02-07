386d0293d11af4e5c95c43b789bb169e09d7bf65
TKachor <tkachor@gmail.com>
2023-02-07 21:26:15 +0200

    feature: add GetCategoriesAsync tests to SqliteInMemoryTest

--- src/Catalog.Tests/Controllers/CategoryControllerSqliteInMemoryTest.cs
+++ src/Catalog.Tests/Controllers/CategoryControllerSqliteInMemoryTest.cs
@@ -33,7 +33,48 @@ public class CategoryControllerSqliteInMemoryTest : IDisposable
 
     CatalogContext CreateContext() => new CatalogContext(_contextOptions);
 
-    public void Dispose() => _connection.Dispose();    
+    public void Dispose() => _connection.Dispose();
+
+
+    [Fact]
+    public async Task GetCategoriesAsync_Returns200Ok_WhenRequestIsSuccess()
+    {
+        using var categoryContext = CreateContext();
+
+        var categoryController = new CategoriesController(categoryContext);
+        var actionResult = await categoryController.GetCategoriesAsync();
+        var okResult = actionResult?.Result as OkObjectResult;
+
+        Assert.Equal(StatusCodes.Status200OK, okResult?.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetCategoriesAsync_ReturnActionResultOfIEnumerableOfBrand_WhenSuccess()
+    {
+        using var categoryContext = CreateContext();
+
+        var categoryController = new CategoriesController(categoryContext);
+        var actionResult = await categoryController.GetCategoriesAsync();
+
+        Assert.IsType<ActionResult<IEnumerable<Category>>>(actionResult);
+    }
+
+    [Fact]
+    public async Task GetCategoriesAsync_ShouldReturnAllBrands_WhenSuccess()
+    {
+        using var categoryContext = CreateContext();
+
+        var categoryController = new BrandsController(categoryContext);
+        var actionResult = await categoryController.GetBrandsAsync();
+
+        var brands = Assert.IsAssignableFrom<IEnumerable<Category>>(actionResult.Value);
+        Assert.Equal(3, brands.Count());
+        Assert.Collection(
+            brands,
+            b => Assert.Equal("Apple", b.Name),
+            b => Assert.Equal("Dell", b.Name),
+            b => Assert.Equal("Lenovo", b.Name));
+    }
 
     private List<CategoryEntity> GetFakeCategoryList()
     {
