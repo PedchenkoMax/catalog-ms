31ddcebd45fcc22f6237fc5cf53e45f591474833
TKachor <tkachor@gmail.com>
2023-02-07 22:50:25 +0200

    feature: add test max price less then zero in PriceRangeAttributeTests

--- src/Catalog.Tests/ValidationAttribute/PriceRangeAttributeTests.cs
+++ src/Catalog.Tests/ValidationAttribute/PriceRangeAttributeTests.cs
@@ -23,10 +23,24 @@ public class PriceRangeAttributeTests
         var results = new List<ValidationResult>();
         var isValid = Validator.TryValidateObject(model, context, results, true);
 
-        Assert.False(isValid);
-        Assert.Single(results);
+        Assert.False(isValid);        
         Assert.Equal("MinPrice must not be less than zero.", results[0].ErrorMessage);
     }
 
-   
+    [Fact]
+    public void MaxPriceLessThanZero_ReturnsValidationError()
+    {
+        var model = new TestModel
+        {
+            MinPrice = 1,
+            MaxPrice = -10
+        };
+
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        Assert.False(isValid);        
+        Assert.Equal("MaxPrice must not be less than zero.", results[0].ErrorMessage);
+    }
 }
