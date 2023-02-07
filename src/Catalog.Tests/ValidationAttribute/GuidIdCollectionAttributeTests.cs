a9b94b2b283b08f7a77792140088e44cdae6c121
TKachor <tkachor@gmail.com>
2023-02-08 00:26:05 +0200

    feature: add tests to check IsValid in GuidIdCollectionAttributeTests

---
+++ src/Catalog.Tests/ValidationAttribute/GuidIdCollectionAttributeTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Catalog.Tests.ValidationAttribute;
+
+public class GuidIdCollectionAttributeTests
+{
+    [Fact]
+    public void IsValid_ValueIsNull_ReturnsTrue()
+    {
+        var attribute = new GuidIdCollectionAttribute();
+
+        var result = attribute.IsValid(null);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsValid_ValueIsNotGuidList_ReturnsFalse()
+    {
+        var attribute = new GuidIdCollectionAttribute();
+
+        var result = attribute.IsValid(new object());
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void IsValid_ValueIsGuidList_AllGuidsAreValid_ReturnsTrue()
+    {
+        var attribute = new GuidIdCollectionAttribute();
+        var guidList = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var result = attribute.IsValid(guidList);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsValid_ValueIsGuidList_ContainsInvalidGuid_ReturnsFalse()
+    {
+        var attribute = new GuidIdCollectionAttribute();
+        var guidList = new List<Guid> { Guid.NewGuid(), Guid.Empty };
+
+        var result = attribute.IsValid(guidList);
+
+        Assert.False(result);
+    }
+
+}
+
+
