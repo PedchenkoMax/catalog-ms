1ada451ae69cf4a386113983f4e4f134e565a370
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-08 10:24:18 +0200

    refactor: remove extra fields from events

--- src/Catalog.API/Events/Product/ProductCreated.cs
+++ src/Catalog.API/Events/Product/ProductCreated.cs
@@ -4,13 +4,10 @@ using Catalog.Infrastructure.Database;
 
 namespace Catalog.API.Events.Product;
 
-public record ProductCreatedEventImage(Guid Id, string ImageUrl, bool IsMain);
-
 public record ProductCreatedEvent(
     Guid Id,
     string Name,
     string Description,
-    List<ProductCreatedEventImage> Images,
     decimal FullPrice,
     decimal Discount,
     int Quantity,
@@ -25,7 +22,6 @@ public record ProductCreatedEvent(
             Id = Id,
             Name = Name,
             Description = Description,
-            Images = Images.Select(i => new ProductImageEntity { Id = i.Id, ImageUrl = i.ImageUrl, IsMain = i.IsMain }).ToList(),
             FullPrice = FullPrice,
             Discount = Discount,
             Quantity = Quantity,
