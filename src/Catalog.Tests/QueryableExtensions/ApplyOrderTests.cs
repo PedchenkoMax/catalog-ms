80c4f1823327ea7e407e68eda9bbc777466a3303
TKachor <tkachor@gmail.com>
2023-02-08 15:48:22 +0200

    feature: add tests with ordering by discount in ApplyOrderTests

---
+++ src/Catalog.Tests/QueryableExtensions/ApplyOrderTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.Tests.QueryableExtensions;
+
+public class ApplyOrderTests
+{
+    [Fact]
+    public void GivenOrderFilterWithOrderByDiscountAndIsDescFalse_ShouldOrderByDiscount()
+    {
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var IsDesc = false;
+        var order = new OrderFilter(OrderByEnum.Discount, IsDesc);
+
+        var result = products.AsQueryable().ApplyOrder(order);
+
+        Assert.Equal(products.OrderBy(p => p.Discount), result);
+    }
+
+    [Fact]
+    public void GivenOrderFilterWithOrderByDiscountAndIsDescTrue_ShouldOrderByDiscountDescending()
+    {
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var IsDesc = true;
+        var order = new OrderFilter(OrderByEnum.Discount, IsDesc);
+
+        var result = products.AsQueryable().ApplyOrder(order);
+
+        Assert.Equal(products.OrderByDescending(p => p.Discount), result);
+    }     
+}
