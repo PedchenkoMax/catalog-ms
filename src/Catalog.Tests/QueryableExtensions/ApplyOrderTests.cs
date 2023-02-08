23bece1100b6312ecbb04b98f1c4b64c75f5a7b5
TKachor <tkachor@gmail.com>
2023-02-08 15:57:25 +0200

    feature: add tests with ordering by fullprice in ApplyOrderTests

--- src/Catalog.Tests/QueryableExtensions/ApplyOrderTests.cs
+++ src/Catalog.Tests/QueryableExtensions/ApplyOrderTests.cs
@@ -24,5 +24,29 @@ public class ApplyOrderTests
         var result = products.AsQueryable().ApplyOrder(order);
 
         Assert.Equal(products.OrderByDescending(p => p.Discount), result);
-    }     
+    }
+
+    [Fact]
+    public void GivenOrderFilterWithOrderByFullPrice_ShouldOrderByFullPrice()
+    {
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var IsDesc = false;
+        var order = new OrderFilter(OrderByEnum.FullPrice, IsDesc);
+
+        var result = products.AsQueryable().ApplyOrder(order);     
+
+        Assert.Equal(products.OrderBy(p => p.FullPrice), result);
+    }
+
+    [Fact]
+    public void GivenOrderFilterWithOrderByFullPriceAndIsDescTrue_ShouldOrderByFullPriceDescending()
+    {
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var IsDesc = true;
+        var order = new OrderFilter(OrderByEnum.FullPrice, IsDesc);
+
+        var result = products.AsQueryable().ApplyOrder(order);
+
+        Assert.Equal(products.OrderByDescending(p => p.FullPrice), result);
+    }    
 }
