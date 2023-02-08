0c243f71548fa9612c9702a42b1011bde4ca9ea2
TKachor <tkachor@gmail.com>
2023-02-08 18:06:12 +0200

    feature: add test to ApplyFilterTests shoul filter by many BrandsId

--- src/Catalog.Tests/QueryableExtensions/ApplyFilterTests.cs
+++ src/Catalog.Tests/QueryableExtensions/ApplyFilterTests.cs
@@ -18,7 +18,10 @@ public class ApplyFilterTests
     [Fact]
     public void ApplyFilter_ShouldFilterByBrandId()
     {        
-        var brandId = new List<Guid>() { SeedDataConstants.BrandApple };
+        var brandId = new List<Guid>() 
+        { 
+            SeedDataConstants.BrandApple 
+        };
         var products = FakeData.GetFakeProductsList().AsQueryable();
         var criteriaFilter = new ProductFilter(default, brandId.ToList(), default, default);
 
@@ -26,5 +29,23 @@ public class ApplyFilterTests
 
         Assert.Equal(6, result.Count());
         Assert.True(result.All(p => p.BrandId == brandId.First()));
+    }
+
+    [Fact]
+    public void ApplyFilter_ShouldFilterByManyBrandIds()
+    {
+        var brandIds = new List<Guid>()
+        {
+            SeedDataConstants.BrandApple,
+            SeedDataConstants.BrandSamsung
+        };
+
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var criteriaFilter = new ProductFilter(default, brandIds, default, default);
+
+        var result = products.ApplyFilter(criteriaFilter);
+
+        Assert.Equal(9, result.Count());
+        Assert.True(result.All(p => brandIds.Contains(p.BrandId)));
     }
 }
