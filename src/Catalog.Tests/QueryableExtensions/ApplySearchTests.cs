4a6075602afa0ee2c617c66ab73a3f769afa4b4a
TKachor <tkachor@gmail.com>
2023-02-08 14:34:19 +0200

    feature: add test with valid query in ApplySearchTests

---
+++ src/Catalog.Tests/QueryableExtensions/ApplySearchTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.Tests.FilterExtensions;
+public class ApplySearchTests
+{
+    [Fact]
+    public void ApplySearch_WithValidSearchFilter_ShouldFilterProducts()
+    {        
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var query = "iphone";
+        var search = new SearchFilter(query);
+        
+        var filteredProducts = products.ApplySearch(search);
+       
+        Assert.NotNull(filteredProducts);
+        Assert.Equal(2, filteredProducts.Count());
+        Assert.True(filteredProducts.All(p => p.Name.ToLower().Contains("iphone")));
+    }
+}
+
