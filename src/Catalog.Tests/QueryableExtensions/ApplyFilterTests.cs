395771a3766f6a9668cba2ff8ed62965f723b106
TKachor <tkachor@gmail.com>
2023-02-08 18:00:14 +0200

    feature: add test to ApplyFilterTests should filter by brand id

--- src/Catalog.Tests/QueryableExtensions/ApplyFilterTests.cs
+++ src/Catalog.Tests/QueryableExtensions/ApplyFilterTests.cs
@@ -3,16 +3,28 @@
 public class ApplyFilterTests
 {
     [Fact]
-    public void ApplyFilter_ShouldFilterByCategoryIds()
+    public void ApplyFilter_ShouldFilterByCategoryId()
     {
-        var NotebookCategoryId = SeedDataConstants.CategoryNotebook;
+        var categoryId = SeedDataConstants.CategoryNotebook;
         var products = FakeData.GetFakeProductsList().AsQueryable();
-        var criteriaFilter = new ProductFilter(NotebookCategoryId, default, default, default);
+        var criteriaFilter = new ProductFilter(categoryId, default, default, default);
         
         var result = products.ApplyFilter(criteriaFilter);
        
         Assert.Equal(6, result.Count());       
-        Assert.True(result.All(p => p.CategoryId == NotebookCategoryId));       
-    }   
+        Assert.True(result.All(p => p.CategoryId == categoryId));       
+    }
 
+    [Fact]
+    public void ApplyFilter_ShouldFilterByBrandId()
+    {        
+        var brandId = new List<Guid>() { SeedDataConstants.BrandApple };
+        var products = FakeData.GetFakeProductsList().AsQueryable();
+        var criteriaFilter = new ProductFilter(default, brandId.ToList(), default, default);
+
+        var result = products.ApplyFilter(criteriaFilter);
+
+        Assert.Equal(6, result.Count());
+        Assert.True(result.All(p => p.BrandId == brandId.First()));
+    }
 }
