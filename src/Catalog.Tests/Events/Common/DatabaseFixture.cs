71209c007309901fb5656aa39546ba518b78d3b7
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-15 16:36:11 +0200

    feature: add global seed data

--- src/Catalog.Tests/Events/Common/DatabaseFixture.cs
+++ src/Catalog.Tests/Events/Common/DatabaseFixture.cs
@@ -1,4 +1,4 @@
-﻿namespace Catalog.Tests.Events.Common;
+namespace Catalog.Tests.Events.Common;
 
 public sealed class DatabaseFixture : IDisposable
 {
@@ -9,12 +9,21 @@ public sealed class DatabaseFixture : IDisposable
             .UseSqlServer(ConnectionString)
             .Options;
 
+    public BrandEntity SeedBrand1 { get; private set; }
+    public BrandEntity SeedBrand2 { get; private set; }
+    public CategoryEntity SeedCategory1 { get; private set; }
+    public CategoryEntity SeedCategory2 { get; private set; }
+    public ProductEntity SeedProduct1 { get; private set; }
+    public ProductEntity Product2 { get; private set; }
+
     public DatabaseFixture()
     {
         using var ctx = new CatalogContext(options);
 
         ctx.Database.EnsureDeleted();
         ctx.Database.EnsureCreated();
+
+        Seed(ctx);
     }
 
     public void Dispose()
@@ -22,6 +31,29 @@ public sealed class DatabaseFixture : IDisposable
         using var ctx = new CatalogContext(options);
 
         ctx.Database.EnsureDeleted();
+    }
+
+    private void Seed(CatalogContext ctx)
+    {
+        SeedBrand1 = new BrandEntity { Id = Guid.NewGuid(), Name = "Brand1", Image = "BrandUrl1" };
+        SeedBrand2 = new BrandEntity { Id = Guid.NewGuid(), Name = "Brand2", Image = "BrandUrl2" };
+
+        SeedCategory1 = new CategoryEntity { Id = Guid.NewGuid(), Name = "Category1", Image = "CategoryUrl1" };
+        SeedCategory2 = new CategoryEntity { Id = Guid.NewGuid(), Name = "Category2", Image = "CategoryUrl2" };
+
+        SeedProduct1 = new ProductEntity
+        {
+            Id = Guid.NewGuid(), CategoryId = SeedCategory1.Id, BrandId = SeedBrand1.Id,
+            Name = "ProductName1", Description = "ProductDescription1", FullPrice = 0, Discount = 0, Quantity = 0, IsActive = false
+        };
+        Product2 = new ProductEntity
+        {
+            Id = Guid.NewGuid(), CategoryId = SeedCategory2.Id, BrandId = SeedBrand2.Id,
+            Name = "ProductName2", Description = "ProductDescription2", FullPrice = 0, Discount = 0, Quantity = 0, IsActive = false
+        };
+
+        ctx.AddRange(SeedBrand1, SeedBrand2, SeedCategory1, SeedCategory2, SeedProduct1, Product2);
+        ctx.SaveChanges();
     }
 }
 
