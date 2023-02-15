e30f0c970f50e63350753158b62b1db4ec6b0af8
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-15 14:23:29 +0200

    feature: add wrapper for testing catalog context

---
+++ src/Catalog.Tests/Events/Common/TestCatalogContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.Tests.Events.Common;
+
+public class TestCatalogContext : CatalogContext
+{
+    public static AutoResetEvent ResetEvent = new(false);
+
+    public TestCatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+    {
+    }
+
+    public override ValueTask DisposeAsync()
+    {
+        ResetEvent.Set();
+        return base.DisposeAsync();
+    }
+}
