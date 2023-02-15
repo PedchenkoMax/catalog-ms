a99ee12a08b078ed7406cc593b4e73251bd1c7c1
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-15 14:22:12 +0200

    feature: add XUnit's fixture for database

---
+++ src/Catalog.Tests/Events/Common/DatabaseFixture.cs
@@ -0,0 +1,31 @@
+﻿namespace Catalog.Tests.Events.Common;
+
+public sealed class DatabaseFixture : IDisposable
+{
+    public const string ConnectionString = "Server=localhost;Database=EventTestDb;User=sa;TrustServerCertificate=true;Trusted_Connection=true;PersistSecurityInfo=true;";
+
+    private readonly DbContextOptions<CatalogContext> options =
+        new DbContextOptionsBuilder<CatalogContext>()
+            .UseSqlServer(ConnectionString)
+            .Options;
+
+    public DatabaseFixture()
+    {
+        using var ctx = new CatalogContext(options);
+
+        ctx.Database.EnsureDeleted();
+        ctx.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        using var ctx = new CatalogContext(options);
+
+        ctx.Database.EnsureDeleted();
+    }
+}
+
+[CollectionDefinition("Database Fixture")]
+public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+{
+}
