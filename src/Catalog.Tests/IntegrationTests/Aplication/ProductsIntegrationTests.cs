45e68f452f46dd14a35c4528327918947b7823a8
TKachor <tkachor@gmail.com>
2023-02-19 14:09:26 +0200

    tetst: add test for products return ok result

---
+++ src/Catalog.Tests/IntegrationTests/Aplication/ProductsIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Tests.IntegrationTests.Aplication;
+
+public class ProductsIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
+{
+    private readonly HttpClient client;
+    public ProductsIntegrationTests(TestingWebAppFactory<Program> factory)
+        => client = factory.CreateClient();
+
+    [Fact]
+    public async Task GetBrandsAsync_ReturnsOkResultWithData()
+    {
+        var response = await client.GetAsync("/api/products");
+        response.EnsureSuccessStatusCode();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
