59790d480059747ee07f82b316b32e7023eec6bc
TKachor <tkachor@gmail.com>
2023-02-19 13:37:19 +0200

    tests: add test for brands with data return all brands

--- src/Catalog.Tests/IntegrationTests/Aplication/BrandsIntegrationTests.cs
+++ src/Catalog.Tests/IntegrationTests/Aplication/BrandsIntegrationTests.cs
@@ -21,5 +21,21 @@ public class BrandsIntegrationTests : IClassFixture<TestingWebAppFactory<Program
         var response = await client.GetAsync("/api/brands");
 
         Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
-    }   
+    }
+
+    [Fact]
+    public async Task GetBrandsAsync_ReturnAllBrands()
+    {
+        var response = await client.GetAsync("/api/brands");
+        response.EnsureSuccessStatusCode();
+
+        var brands = await response.Content.ReadFromJsonAsync<List<Brand>>();
+
+        Assert.Equal(4, brands.Count);
+        Assert.Collection(brands,
+            item => Assert.Equal("Lenovo", item.Name),
+            item => Assert.Equal("Samsung", item.Name),
+            item => Assert.Equal("Apple", item.Name),
+            item => Assert.Equal("Lg", item.Name));
+    }
 }
