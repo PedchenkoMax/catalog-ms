a3880a669f087dcc8de4d0f6297913d878fb3a5d
TKachor <tkachor@gmail.com>
2023-02-19 13:34:52 +0200

    tests: add test for brands return ok result

---
+++ src/Catalog.Tests/IntegrationTests/Aplication/BrandsIntegrationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Tests.IntegrationTests.API;
+
+public class BrandsIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
+{
+    private readonly HttpClient client;
+    public BrandsIntegrationTests(TestingWebAppFactory<Program> factory)
+        => client = factory.CreateClient();
+
+    [Fact]
+    public async Task GetBrandsAsync_ReturnsOkResultWithData()
+    {
+        var response = await client.GetAsync("/api/brands");
+        response.EnsureSuccessStatusCode();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }    
+}
