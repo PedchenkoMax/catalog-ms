38e0d601d75212ef4662aa6ed50568015a7c9c39
PedchenkoMax <pedchenko.maxs@gmail.com>
2023-02-24 20:37:13 +0200

    feature: add smoke test

---
+++ src/Catalog.Tests/LoadTests/smoke.js
@@ -0,0 +1,25 @@
+﻿// Smoke Load Testing
+// This type of testing is done to quickly verify if the system is working as expected under a light load.
+// Total: 3m
+
+import {loadTest} from './common/scenarios.js';
+
+export const options = {
+    stages: [
+        {duration: '15s', target: 10},
+        {duration: '45s', target: 25},
+        {duration: '1m', target: 50},
+        {duration: '45s', target: 25},
+        {duration: '15s', target: 0}
+    ],
+    thresholds: {
+        http_req_failed: ['rate < 0.001'], // 0.1%
+        http_req_duration: ['p(95) < 150'],
+
+        get_products_duration: ['p(95) < 75'],
+        get_brands_duration: ['p(95) < 25'],
+        get_categories_duration: ['p(95) < 25']
+    }
+};
+
+export default loadTest;
