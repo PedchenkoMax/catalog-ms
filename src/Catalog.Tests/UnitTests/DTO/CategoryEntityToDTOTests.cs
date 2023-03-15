bd598ae901d493cee28967eacf64ca043ab04c74
TKachor <tkachor@gmail.com>
2023-03-15 15:24:16 +0200

    tests: add unit tests for category entity to dto

---
+++ src/Catalog.Tests/UnitTests/DTO/CategoryEntityToDTOTests.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.DTO;
+using Xunit;
+
+namespace Catalog.Tests.UnitTests.ToDTO;
+
+public class CategoryEntityToDTOTests 
+{
+    [Fact]
+    public void CategoryEntityToDTOFirst_ReturnsCorrectDTOObject() 
+    {
+        var categoryEntity = FakeData.GetFakeBrandsList().First();
+
+        var brandDto = categoryEntity.ToDTO();
+
+        Assert.Equal(categoryEntity.Id, brandDto.BrandId);
+        Assert.Equal(categoryEntity.Name, brandDto.Name);
+        Assert.Equal(categoryEntity.Image, brandDto.Image);
+    }
+
+    [Fact]
+    public void CategoryEntityToDTOLast_ReturnsCorrectDTOObject() 
+    {
+        var categoryEntity = FakeData.GetFakeBrandsList().Last();
+
+        var brandDto = categoryEntity.ToDTO();
+
+        Assert.Equal(categoryEntity.Id, brandDto.BrandId);
+        Assert.Equal(categoryEntity.Name, brandDto.Name);
+        Assert.Equal(categoryEntity.Image, brandDto.Image);
+    }
+}
