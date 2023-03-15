2db488bff878176061dc91a1b3d255621ff55717
TKachor <tkachor@gmail.com>
2023-03-15 15:18:08 +0200

    tests: add unit tests for barnd entity to dto

---
+++ src/Catalog.Tests/UnitTests/DTO/BrandEntityToDTOTests.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.DTO;
+using Xunit;
+
+namespace Catalog.Tests.UnitTests.ToDTO;
+
+public class BrandEntityToDTOTests 
+{
+    [Fact]
+    public void BrandEntityToDTOFirst_ReturnsCorrectDTOObject() 
+    {
+        var brandEntity = FakeData.GetFakeBrandsList().First();
+
+        var brandDto = brandEntity.ToDTO();
+
+        Assert.Equal(brandEntity.Id, brandDto.BrandId);
+        Assert.Equal(brandEntity.Name, brandDto.Name);
+        Assert.Equal(brandEntity.Image, brandDto.Image);
+    }
+
+    [Fact]
+    public void BrandEntityToDTOLast_ReturnsCorrectDTOObject() 
+    {
+        var brandEntity = FakeData.GetFakeBrandsList().Last();
+
+        var brandDto = brandEntity.ToDTO();
+
+        Assert.Equal(brandEntity.Id, brandDto.BrandId);
+        Assert.Equal(brandEntity.Name, brandDto.Name);
+        Assert.Equal(brandEntity.Image, brandDto.Image);
+    }
+}
